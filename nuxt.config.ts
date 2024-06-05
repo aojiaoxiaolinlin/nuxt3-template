26224caa3d98ee4e8a4631600d86ef43a045a1cf
傲娇小霖霖 <1589861957@qq.com>
2024-06-06 06:44:34 +0800

    nuxt3-template

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@unocss/nuxt', "@nuxt/eslint"],
+  eslint: {
+    config:{
+      stylistic: true,
+      standalone: true,
+    }
+  },
+  devtools: { enabled: true }
+})
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1
+      m-2
+      bg-sky
+      text-xl
+      text-black
+      hover:bg-slate
+    >
+      Home Page
+    </h1>
+    {{ count }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const count = ref(0)
+</script>
+
+<style scoped></style>
