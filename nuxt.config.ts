22442968099d0dce202d4340e559ea83c6215a4d
傲娇小霖霖 <1589861957@qq.com>
2024-07-27 21:39:21 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@unocss/nuxt', '@nuxt/eslint'],
+
   eslint: {
     config: {
       stylistic: true,
       standalone: true,
     },
   },
+
   devtools: { enabled: true },
+  compatibilityDate: '2024-07-27',
 })
